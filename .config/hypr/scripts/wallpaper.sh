c0d13f127510ffdecddff813c6af85e0dd1daab2
offlinebot <offlinebot@fedora.fritz.box>
2024-06-17 20:24:15 +0200

    wallpaper

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-~/Apps/Scripts/swww img ~/Pictures/slideshow/abstract.jpg --transition-step 100 --transition-fps 60
 
+#~/Apps/Scripts/swww img ~/Pictures/slideshow/abstract.jpg --transition-step 100 --transition-fps 60
+#
+#SLEEP_TIMER=100
+#
+#while true; do
+#    ~/Apps/Scripts/swww img ~/Pictures/slideshow/abstract.jpg --transition-step 50 --transition-fps 60 --transition-type center
+#    sleep $SLEEP_TIMER
+#    ~/Apps/Scripts/swww img ~/Pictures/slideshow/red_city.png --transition-step 50 --transition-fps 60 --transition-type center
+#    sleep $SLEEP_TIMER
+#    ~/Apps/Scripts/swww img ~/Pictures/slideshow/snow_area.jpg --transition-step 50 --transition-fps 60 --transition-type center
+#    sleep $SLEEP_TIMER
+#done
+
+
+IMG_DIR=~/Pictures/slideshow
+
+# in seconds
 SLEEP_TIMER=100
 
 while true; do
-    ~/Apps/Scripts/swww img ~/Pictures/slideshow/abstract.jpg --transition-step 50 --transition-fps 60 --transition-type center
-    sleep $SLEEP_TIMER
-    ~/Apps/Scripts/swww img ~/Pictures/slideshow/red_city.png --transition-step 50 --transition-fps 60 --transition-type center
-    sleep $SLEEP_TIMER
-    ~/Apps/Scripts/swww img ~/Pictures/slideshow/snow_area.jpg --transition-step 50 --transition-fps 60 --transition-type center
-    sleep $SLEEP_TIMER
+    for image in "$IMG_DIR"/*; do
+        ~/Apps/Scripts/swww img "$image" --transition-step 60 --transition-fps 60 --transition-type center
+        sleep $SLEEP_TIMER
+    done
 done
 
