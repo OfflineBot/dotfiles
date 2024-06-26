1f4b5e69f499630951e681e7f1bc5b3f1b28347a
offlinebot <offlinebot@fedora.fritz.box>
2024-06-26 10:22:27 +0200

    new

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,12 +1,3 @@
---require'nvim-tree'.setup {
---    git = {
---        enable = true,
---        ignore = false,
---        timeout = 400,
---    },
---}
---
--- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -17,21 +8,41 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup()
 
 -- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+--require("nvim-tree").setup({
+--    sort = {
+--        sorter = "case_sensitive",
+--    },
+--    view = {
+--        width = 30,
+--    },
+--    renderer = {
+--        group_empty = true,
+--    },
+--    filters = {
+--        dotfiles = true,
+--    },
+--})
 
+
+require'nvim-tree'.setup {
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        }
+      },
+      show = {
+          folder_arrow = false,
+      }
+    }
+  }
+}
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
 vim.cmd [[command! NTT :NvimTreeToggle]]
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-My dotfiles with stow
+My dotfiles with stow. <br>
+It the main setup i mainly use!
