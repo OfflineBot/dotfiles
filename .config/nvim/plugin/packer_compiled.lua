8834973a04e8e52ee4dd10d26f8f08107e1723fe
offlinebot <offlinebot@fedora.fritz.box>
2024-07-09 23:55:43 +0200

    new

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -144,6 +144,11 @@ _G.packer_plugins = {
     path = "/home/offlinebot/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["minimap.vim"] = {
+    loaded = true,
+    path = "/home/offlinebot/.local/share/nvim/site/pack/packer/start/minimap.vim",
+    url = "https://github.com/wfxr/minimap.vim"
+  },
   nerdcommenter = {
     loaded = true,
     path = "/home/offlinebot/.local/share/nvim/site/pack/packer/start/nerdcommenter",
